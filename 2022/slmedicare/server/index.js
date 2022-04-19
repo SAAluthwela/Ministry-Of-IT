68dca46453c864779be81cb20fa6e392f88da605
SAAluthwela <supunakalanka.git@gmail.com>
2022-04-19 19:44:52 +0530

    initialization

---
+++ 2022/slmedicare/server/index.js
@@ -0,0 +1,8 @@
+/*older version
+  const express = require('express');
+*/
+
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
